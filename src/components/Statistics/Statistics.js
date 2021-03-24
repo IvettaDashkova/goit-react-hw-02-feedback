b23550fbdcaa6d3ac9601ef89f65d0e35825397f
IvettaDashkova <ivettazakonno@gmail.com>
2021-03-25 01:38:57 +0200

    fix task 1

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,7 +5,7 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
   <ul className={styles.list}>
     <li className={styles.item}>good: {good}</li>
     <li className={styles.item}>neutral: {neutral}</li>
-    <li className={styles.item}>bed: {bad}</li>
+    <li className={styles.item}>bad: {bad}</li>
     <li className={styles.item}>total: {total}</li>
     <li className={styles.item}>positive feedback: {positivePercentage}%</li>
   </ul>
